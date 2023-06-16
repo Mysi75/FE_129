810f18505c5fde2e4cee366db3d84deed4ba545e
iryna tsiaroshkina <irisa201@mail.ru>
2023-06-16 23:10:14 +0300

    deploy in Firebase

--- organizer/organize/src/store/action-creatos/organize.ts
+++ organizer/organize/src/store/action-creatos/organize.ts
@@ -3,50 +3,27 @@ import { OrganizeActionTypes, OrganizeActions, ITask } from "../../type/organize
 import {child, get, set, ref, update} from "firebase/database";
 import { db } from "../../utils/dbConfig";
 
-// export const  getOrganize = (uId: string | null | undefined) => {
-//     return async (dispatch: Dispatch<OrganizeActions>) => {
-//         try {
-//             dispatch({type: OrganizeActionTypes.FETCH_ORGANIZE});
-//             const response = await get(child(ref(db), `organize/$(uId)`));
-//             if (response.exists()){
-//                 dispatch({
-//                     type:OrganizeActionTypes.GET_ORGANIZE,
-//                     payload: response.val()
-//                 });
-//             }else{
-//                 throw new Error()
-//             }
-//         }catch (error) {
-//             dispatch({
-//                 type: OrganizeActionTypes.ERROR_ORGANIZE,
-//                 payload: 'Произошла ошибка',
-//             });
-//         }
-//     }
-// }
-
-    export const getOrganize = (uId: string | null | undefined) => {
+ export const  getOrganize = (uId: string | null | undefined) => {
     return async (dispatch: Dispatch<OrganizeActions>) => {
-        try {
-            dispatch({ type: OrganizeActionTypes.FETCH_ORGANIZE });
+         try {
+             dispatch({type: OrganizeActionTypes.FETCH_ORGANIZE});
             const response = await get(child(ref(db), `organize/${uId}`));
-            if (response.exists()) {
-                dispatch({
-                    type: OrganizeActionTypes.GET_ORGANIZE,
-                    payload: response.val()
-                });
-            } else {
-                throw new Error()
-            }
-        } catch (error) {
-            dispatch({
-                type: OrganizeActionTypes.ERROR_ORGANIZE,
-                payload: 'Произошла ошибка',
-            });
-        }
-    }
-}
-
+             if (response.exists()){
+                 dispatch({
+                     type:OrganizeActionTypes.GET_ORGANIZE,
+                     payload: response.val()
+                 });
+             }else{
+                 throw new Error()
+             }
+        }catch (error) {
+             dispatch({
+                 type: OrganizeActionTypes.ERROR_ORGANIZE,
+                 payload: 'Произошла ошибка',
+             });
+         }
+     }
+ }
 
  export const setOrganize = (
      uId: string | null | undefined, 
@@ -81,52 +58,29 @@ import { db } from "../../utils/dbConfig";
      }
  }
 
-// export const editOrganize = (
-//     uId: string | null | undefined,
-//     task: ITask)  => {
-//         return async (dispatch: Dispatch<OrganizeActions>) => {
-//             dispatch({type: OrganizeActionTypes.FETCH_ORGANIZE});
-//             try {
-//                 const response = await get(child(ref(db),`organize/$(uId)`));
-//                 if(response.exists()){
-//                     const oldTasc = response.val();
-//                     oldTasc.tasks = [...oldTasc.tasks, task];
-//                     await update(ref(db, `organize/${uId}`), oldTasc);
-//                     dispatch({type: OrganizeActionTypes.EDIT_TASK, payload: 'Данные обновлены'});
-//                 }else{
-//                     throw new Error();
-//                 }
-//             } catch (error) {
-//                 dispatch({
-//                     type: OrganizeActionTypes.ERROR_ORGANIZE,
-//                      payload: 'Не удалось обновить данные'
-//                 });
-//             }
-//         }
+ export const editOrganize = (
+     uId: string | null | undefined,
+     task: ITask)  => {
+         return async (dispatch: Dispatch<OrganizeActions>) => {
+             dispatch({type: OrganizeActionTypes.FETCH_ORGANIZE});
+             try {
+                const response = await get(child(ref(db),`organize/$(uId)`));
+                 if(response.exists()){
+                     const oldTasc = response.val();
+                     oldTasc.tasks = [...oldTasc.tasks, task];
+                     await update(ref(db, `organize/${uId}`), oldTasc);
+                     dispatch({type: OrganizeActionTypes.EDIT_TASK, payload: 'Данные обновлены'});
+                 }else{
+                     throw new Error();
+                 }
+             } catch (error) {
+                 dispatch({
+                     type: OrganizeActionTypes.ERROR_ORGANIZE,
+                      payload: 'Не удалось обновить данные'
+                 });
+             }
+         }
 
-// }
+ }
 
-export const editOrganize = (
-    uId: string | null | undefined,
-    task: ITask) => {
-    return async (dispatch: Dispatch<OrganizeActions>) => {
-        dispatch({ type: OrganizeActionTypes.FETCH_ORGANIZE });
-        try {
-            const response = await get(child(ref(db), `organize/${uId}`));
-            if (response.exists()) {
-                const oldTasc = response.val();
-                oldTasc.tasks = [...oldTasc.tasks, task];
-                await update(ref(db, `organize/${uId}`), oldTasc);
-                dispatch({ type: OrganizeActionTypes.EDIT_TASK, payload: 'Данные обнавлены' });
-            } else {
-                throw new Error();
-            }
-        } catch (error) {
-            dispatch({
-                type: OrganizeActionTypes.ERROR_ORGANIZE,
-                payload: 'Не удалось обнавить данные'
-            });
-        }
-    }
-}
 
